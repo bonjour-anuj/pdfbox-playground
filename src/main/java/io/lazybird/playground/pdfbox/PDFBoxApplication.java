cef3b1a82150159da0174767e34fff99524409e9
Anuj Bhatnagar <bonjour.anuj@gmail.com>
2020-08-30 16:45:19 +0530

    Initial Commit

---
+++ src/main/java/io/lazybird/playground/pdfbox/PDFBoxApplication.java
@@ -0,0 +1,20 @@
+package io.lazybird.playground.pdfbox;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Logger;
+import org.apache.pdfbox.pdmodel.PDDocument;
+
+/**
+ * @author Anuj Bhatnagar
+ **/
+public class PDFBoxApplication {
+
+  private static Logger logger = Logger.getLogger(PDFBoxApplication.class.getName());
+
+  public static void main(String[] args) {
+
+
+  }
+
+}
